084f918b7dd6230f8ef12ce3191ef21069c7b719
Charles Dete <charlesdete52@gmail.com>
2025-04-17 23:22:35 +0300

    Error handling when login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/userAuth.css";
-import { collection, doc, getDoc } from "@firebase/firestore";
+import { doc, getDoc } from "@firebase/firestore";
 import { db } from "../firebase";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [User, setUser] = useState(null);
   const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -28,14 +27,17 @@ export default function Login() {
       setError("Email is invalid. Please try again.");
       return;
     }
+    if (validateEmail(password) == false) {
+      setError("Password is invalid. Please try again.");
+      return;
+    }
 
     setError("");
     setLoading(true);
     const response = await login(email, password);
+    console.log(response);
 
     if (response?.user !== "undefined" && response?.user !== null) {
-      console.log(response.user.uid);
-
       try {
         // Reference the user document directly using doc()
         const userDocRef = doc(db, "users", response.user.uid);
@@ -44,7 +46,6 @@ export default function Login() {
 
         if (docSnapshot.exists()) {
           const theUser = docSnapshot.data(); // Extract user data
-          setUser(theUser); // Store in state
 
           // Store in localStorage
           localStorage.setItem("user", JSON.stringify(theUser));
@@ -55,6 +56,7 @@ export default function Login() {
         }
       } catch (e) {
         console.error("There is an error fetching the logged-in user", e);
+        setLoading(false);
       }
 
       navigate("/Home");
@@ -68,9 +70,20 @@ export default function Login() {
 
   return (
     <>
-      {error && <div>{error}</div>}
       <form className="form">
         <h5>Sign in</h5>
+        {error && (
+          <div
+            style={{
+              color: "red",
+              fontSize: "8px",
+              backgroundColor: "#ff000033",
+            }}
+          >
+            {error}
+          </div>
+        )}
+
         <div className="input-shield">
           <input
             value={email}
@@ -93,11 +106,46 @@ export default function Login() {
         </div>
 
         <button
+          style={{ width: "200px", height: "40px", borderRadius: "10px" }}
           disabled={loading}
           type="submit"
           onClick={(e) => handleSubmit(e)}
         >
-          {loading ? "Signing in..." : "Sign In"}
+          {loading ? (
+            <svg
+              style={{
+                width: "30px",
+                height: "30px",
+                display: "inline-block",
+              }}
+              version="1.1"
+              id="L9"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 100 100"
+              enable-background="new 0 0 0 0"
+              xmlSpace="preserve"
+            >
+              <path
+                fill="#fff"
+                d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="rotate"
+                  dur="1s"
+                  from="0 50 50"
+                  to="360 50 50"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </svg>
+          ) : (
+            "Sign In"
+          )}
         </button>
         <div className="w-100 text-center mt-2">
           Need an account? <Link to="/signup">Sign Up</Link>
